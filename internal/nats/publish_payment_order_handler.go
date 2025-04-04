4341767f04fc7f7d31b94a26307653e0cac722b5
milo33 <65894437+milo1150@users.noreply.github.com>
2025-04-04 18:44:24 +0700

    fix: string message should not be capatalize

--- internal/nats/publish_payment_order_handler.go
+++ internal/nats/publish_payment_order_handler.go
@@ -12,12 +12,12 @@ import (
 func PublishCreatePaymentOrderHandler(js jetstream.JetStream, ctx context.Context, payload ps.PublishCreatedPaymentOrderPayload) error {
 	data, err := json.Marshal(payload)
 	if err != nil {
-		return fmt.Errorf("Failed to parse payment_order.created payload: %w", err)
+		return fmt.Errorf("failed to parse payment_order.created payload: %w", err)
 	}
 
 	_, err = js.Publish(ctx, "payment_order.created", data)
 	if err != nil {
-		return fmt.Errorf("Failed to publish payment_order.created: %w", err)
+		return fmt.Errorf("failed to publish payment_order.created: %w", err)
 	}
 
 	return nil
