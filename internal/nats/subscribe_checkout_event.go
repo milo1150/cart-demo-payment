bfd8f63be3dd1896ba813a85f4d53f7908711bd8
milo33 <65894437+milo1150@users.noreply.github.com>
2025-04-17 21:56:13 +0700

    fix: null consumer

--- internal/nats/subscribe_checkout_event.go
+++ internal/nats/subscribe_checkout_event.go
@@ -13,17 +13,36 @@ func SubscribeCheckoutEvent(js jetstream.JetStream, log *zap.Logger, db *gorm.DB
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	cons, err := js.CreateOrUpdateConsumer(ctx, "CHECKOUT", jetstream.ConsumerConfig{
-		Durable:       "CREATED_CHECKOUT_CONS",
-		AckPolicy:     jetstream.AckExplicitPolicy,
-		FilterSubject: "checkout.created",
-		AckWait:       5 * time.Second,
-	})
-	if err != nil {
-		log.Error("Failed to create CreateConsumer", zap.Error(err))
-	}
+	tick := time.NewTicker(2 * time.Second)
+	ok := make(chan bool)
+	var consumer jetstream.Consumer
 
-	cons.Consume(func(msg jetstream.Msg) {
+	go func() {
+		defer tick.Stop()
+		for range tick.C {
+			cons, err := js.CreateOrUpdateConsumer(ctx, "CHECKOUT", jetstream.ConsumerConfig{
+				Durable:       "CREATED_CHECKOUT_CONS",
+				AckPolicy:     jetstream.AckExplicitPolicy,
+				FilterSubject: "checkout.created",
+				AckWait:       5 * time.Second,
+			})
+
+			if err != nil {
+				log.Error("Failed to create checkout.created consumer", zap.Error(err))
+				continue
+			}
+
+			// Create consumer
+			log.Info("create CREATED_CHECKOUT_CONS consumer OK")
+			consumer = cons
+			ok <- true
+			return
+		}
+	}()
+
+	<-ok
+
+	consumer.Consume(func(msg jetstream.Msg) {
 		err := SubscribeCheckoutHandler(log, db, msg, js)
 		if err != nil {
 			log.Error("Failed to create payment order", zap.Error(err))
